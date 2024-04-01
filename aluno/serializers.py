1ac1cb7c28ba95261c1445f0cac397ddc6c86dab
Beatriz M <125318073+beatrizline@users.noreply.github.com>
2024-04-01 20:05:08 -0300

    Delete aluno directory

--- aluno/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework.serializers import ModelSerializer
-from aluno.models import Aluno
-
-class AlunoSerializer(ModelSerializer):
-    class Meta:
-        model = Aluno
-        fields=['nome', 'foto', 'sexo', 'dt_nasc', 'telefone', 'cpf']
